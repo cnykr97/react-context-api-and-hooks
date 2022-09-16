e9cbe8fe96eeae18c82370459127e8fec8e82438
Can Yakar <canyakar9797@gmail.com>
2022-09-16 20:47:35 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Booklist from "./components/Booklist";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <Booklist></Booklist>
+    </div>
+  );
+}
+
+export default App;
