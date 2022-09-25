6f7327643dcb83285e7bfe43a27fde8ca8b653c9
Can Yakar <canyakar9797@gmail.com>
2022-09-25 18:18:34 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Navbar from "./components/Navbar";
 import Booklist from "./components/Booklist";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <Booklist></Booklist>
+      <ThemeContextProvider>
+        <Navbar></Navbar>
+        <Booklist></Booklist>
+      </ThemeContextProvider>
     </div>
   );
 }
